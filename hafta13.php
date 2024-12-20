768bf012fd7079ce23ca3a5117bee9f5440cb1e3
asefagultekin <agultekin@hacettepe.edu.tr>
2024-12-20 10:20:32 +0300

    Update hafta13.php

--- hafta13.php
+++ hafta13.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test";
+$dbname = "kisiler";
 
 // Bağlantıyı oluştur
 $conn = new mysqli($servername, $username, $password, $dbname);
